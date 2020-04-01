1ba9d6665f6aa4b8517523bebaf08e1c60aefd06
1stmuse <olysegs@gmail.com>
2020-04-01 07:42:22 +0100

    Updates

---
+++ precache-manifest.4bf93a536e8f12faf5d723f91a400419.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f784d363c3774f4cb7a6f2f36d94ec6c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "05a4249791670951747b",
+    "url": "/portfolio/static/css/main.af6129d6.chunk.css"
+  },
+  {
+    "revision": "0af4c8ad9686adfc4ef1",
+    "url": "/portfolio/static/js/2.ea289d48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ea289d48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05a4249791670951747b",
+    "url": "/portfolio/static/js/main.824cee38.chunk.js"
+  },
+  {
+    "revision": "1e74db4740f99c68a0bf",
+    "url": "/portfolio/static/js/runtime-main.b37861ed.js"
+  }
+]);

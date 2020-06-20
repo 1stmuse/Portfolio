c579ac51d5b79a609b725fd1e220eee0d38af717
1stmuse <olysegs@gmail.com>
2020-06-20 15:23:24 -0400

    removed the header component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import React, {Component} from 'react';
 import './App.css';
 import Particles from './components/Particle'
-import {Header} from './components/header/Header'
 import {About} from './components/about/About'
 import {Tech} from './components/tech/Tech'
 
@@ -16,13 +15,8 @@ class App extends Component  {
     }
   }
 
-  clickTech =()=>{
-    this.setState({
-      about : !this.state.about
-    })
-  }
+
   render(){
-    const {tech, about} = this.state
     return (
       <div style={{position:'relative', height:'100vh'}}>
         <Particles>
@@ -30,10 +24,9 @@ class App extends Component  {
         </Particles>
         <Header/>
         <div className='app'>
-          <div className={about?'about-comp': 'about-anim'}><About/> </div>
-          <div className={tech?'tech-anim': 'tech-comp'}><Tech/></div>
+          <div> <About/> </div>
+          <div> <Tech/></div>
         </div>
-        <button onClick={this.clickTech} >home</button>
       </div>
     );
   }
